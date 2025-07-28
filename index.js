4426923ca3db248ff7cd022b63da8352ea82d7e7
surajj1699 <152787331+surajj1699@users.noreply.github.com>
2025-07-28 23:06:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const io = require("socket.io")(server, {
-	cors: {
-		origin: "*",
-		methods: [ "GET", "POST" ]
-	}
-});
+import App from './App';
+import { ContextProvider } from './Context';
 
-app.use(cors());
+import './styles.css';
 
-const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-	res.send('Running');
-});
-
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
-
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	});
-
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
-});
-
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById('root'),
+);
